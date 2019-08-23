10914259cdb403cbb84808d7db583a9d633b11fe
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-08-23 15:03:53 -0700

    configures express session for cookie management

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session')
 var logger = require('morgan');
 
 require('dotenv').config();
@@ -22,6 +23,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secet:'CoinAgora',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
